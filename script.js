cb6ee1a2e331d89f47431664a0b6f9ff15a573d2
jesse <itsjessef@gmail.com>
2022-09-11 19:39:05 -0700

    handle submit from search form

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ nav.style.display = 'none';
 let pageNumber = 0;
 
 // Event listeners to control the functionality
+searchForm.addEventListener('submit', submitSearch);
+
+function submitSearch(e) {
+  pageNumber = 0;
+  fetchResults(e);
+}
+
+function fetchResults(e) {
+  // Use preventDefault() to stop the form submitting
+  e.preventDefault();
+
+  // Assemble the full URL
+  let url = `${baseURL}?api-key=${key}&page=${pageNumber}&q=${searchTerm.value}&fq=document_type:("article")`;
+
+  if (startDate.value !== '') {
+    url = `${url}&begin_date=${startDate.value}`;
+  };
+
+  if (endDate.value !== '') {
+    url = `${url}&end_date=${endDate.value}`;
+  };
+}
