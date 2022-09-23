9ccbf2d33b1312e80badc5f2311c370606274839
jesse <itsjessef@gmail.com>
2022-09-22 19:34:44 -0700

    display the data

--- script.js
+++ script.js
@@ -47,4 +47,54 @@ function fetchResults(e) {
     .then((response) => response.json())
     .then((json) => displayResults(json))
     .catch((error) => console.error(`Error fetching data: ${error.message}`));
+
+  function displayResults(json) {
+    while (section.firstChild) {
+      section.removeChild(section.firstChild);
+    }
+
+    const articles = json.response.docs;
+
+    nav.style.display = articles.length === 10 ? 'block' : 'none';
+
+    if (articles.length === 0) {
+      const para = document.createElement('p');
+      para.textContent = 'No results returned.'
+      section.appendChild(para);
+    } else {
+      for (const current of articles) {
+        const article = document.createElement('article');
+        const heading = document.createElement('h2');
+        const link = document.createElement('a');
+        const img = document.createElement('img');
+        const para1 = document.createElement('p');
+        const keywordPara = document.createElement('p');
+        keywordPara.classList.add('keywords');
+
+        console.log(current);
+
+        link.href = current.web_url;
+        link.textContent = current.headline.main;
+        para1.textContent = current.snippet;
+        keywordPara.textContent = 'Keywords: ';
+        for (const keyword of current.keywords) {
+          const span = document.createElement('span');
+          span.textContent = `${keyword.value} `;
+          keywordPara.appendChild(span);
+        }
+
+        if (current.multimedia.length > 0) {
+          img.src = `http://www.nytimes.com/${current.multimedia[0].url}`;
+          img.alt = current.headline.main;
+        }
+
+        article.appendChild(heading);
+        heading.appendChild(link);
+        article.appendChild(img);
+        article.appendChild(para1);
+        article.appendChild(keywordPara);
+        section.appendChild(article);
+      }
+    }
+  };
 }
