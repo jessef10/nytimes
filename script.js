1909d1b6ff890495f834254dac72993388d2ccbc
jesse <itsjessef@gmail.com>
2022-09-28 12:55:51 -0700

    add event listener for previous and next buttons

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ let pageNumber = 0;
 
 // Event listeners to control the functionality
 searchForm.addEventListener('submit', submitSearch);
+nextBtn.addEventListener('click', nextPage);
+previousBtn.addEventListener('click', previousPage);
 
 
 function submitSearch(e) {
